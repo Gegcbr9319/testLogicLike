918ce6e70f2a002f622bac49292cc2a25ca4e877
gegcbr9319 <gegcbr9319@google.com>
2024-04-22 22:27:48 +0300

    feat: ready structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import styles from './App.module.scss';
+
+export const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <h2>Start</h2>
+     </>
+  )
+}
+
+
